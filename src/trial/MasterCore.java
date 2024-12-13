58619a317814ce1eb37320cdb3ef247b80a2cf3c
Fatma Osama <fosama1570@gmail.com>
2024-12-13 19:38:12 +0200

    C:\Users\fosam\.jdks\openjdk-22.0.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2024.1.1\lib\idea_rt.jar=59131:C:\Program Files\JetBrains\IntelliJ IDEA 2024.1.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\fosam\OneDrive\Desktop\Uni\Semester 3\Operating Systems\OSPROJECTrial\out\production\OSPROJECTrial" trial.Main Enter value for a: 8 aValuesMap: {1=8} bValuesMap: {} otherValuesMap: {} Enter value for b: 7 aValuesMap: {1=8} bValuesMap: {1=7} otherValuesMap: {} Enter value for a: 6 aValuesMap: {1=8, 2=6} bValuesMap: {1=7} otherValuesMap: {} Enter value for b: 5 aValuesMap: {1=8, 2=6} bValuesMap: {1=7, 2=5} otherValuesMap: {} Enter value for a: 4 aValuesMap: {1=8, 2=6, 3=4} bValuesMap: {1=7, 2=5} otherValuesMap: {} Enter value for b: 3 aValuesMap: {1=8, 2=6, 3=4} bValuesMap: {1=7, 2=5, 3=3} otherValuesMap: {} Ready Queue: Process{ processId= 1[Next Instruction: assign z add a b] } Process{ processId= 2[Next Instruction: assign c subtract a b] } Process{ processId= 3[Next Instruction: print b] } Core 1 executing Process 2 Core 0 executing Process 1 Performing add with values: 8 and 8 aValuesMap: {1=8, 2=6, 3=4} bValuesMap: {1=7, 2=5, 3=3} otherValuesMap: {z=16} Exception in thread "Thread-1" java.lang.IllegalStateException: Value for variable s is not set for process ID 2 	at trial.SlaveCore.getOperandValue(SlaveCore.java:176) 	at trial.SlaveCore.performArithmetic(SlaveCore.java:128) 	at trial.SlaveCore.executeInstruction(SlaveCore.java:106) 	at trial.SlaveCore.executeCurrentProcess(SlaveCore.java:71) 	at trial.SlaveCore.run(SlaveCore.java:57) 1z = 16 Core 0 quantum expired for Process 1 Ready Queue: Process 3 [Next: print b] Core 0 executing Process 3 3b = 3 Core 0 completed Process 3
    Process finished with exit code 130

--- src/trial/MasterCore.java
+++ src/trial/MasterCore.java
@@ -1,3 +1,4 @@
+// MasterCore.java
 package trial;
 
 import java.util.HashMap;
@@ -7,6 +8,7 @@ import java.util.Queue;
 public class MasterCore {
     private final HashMap<Integer, Integer> aValuesMap = new HashMap<>();
     private final HashMap<Integer, Integer> bValuesMap = new HashMap<>();
+    private final HashMap<Character, Integer> otherValuesMap = new HashMap<>();
     private final Queue<Process> readyQueue = new LinkedList<>();
     private final SlaveCore[] slaveCores;
 
@@ -41,6 +43,8 @@ public class MasterCore {
             aValuesMap.put(process.getProcessId(), value);
         } else if (variable == 'b') {
             bValuesMap.put(process.getProcessId(), value);
+        } else {
+            otherValuesMap.put(variable, value);
         }
         printValueMaps();
     }
@@ -58,16 +62,18 @@ public class MasterCore {
             return aValuesMap.values().stream().findFirst().orElse(null);
         } else if (variable == 'b') {
             return bValuesMap.values().stream().findFirst().orElse(null);
+        } else {
+            return otherValuesMap.get(variable);
         }
-        return null;
     }
 
     private void printValueMaps() {
         System.out.println("aValuesMap: " + aValuesMap);
         System.out.println("bValuesMap: " + bValuesMap);
+        System.out.println("otherValuesMap: " + otherValuesMap);
     }
 
-        public void printReadyQueue() {
+    public void printReadyQueue() {
         System.out.println("Ready Queue:");
         for (Process process : readyQueue) {
             System.out.println(process);
@@ -82,8 +88,6 @@ public class MasterCore {
 
 
 
-
-//// MasterCore.java
 //package trial;
 //
 //import java.util.HashMap;
@@ -91,22 +95,15 @@ public class MasterCore {
 //import java.util.Queue;
 //
 //public class MasterCore {
-//    private final SharedMemory sharedMemory;
-//    private final int coreCount;
-//    private final Queue<Process> readyQueue;
-//    private final HashMap<Integer, Integer> aValuesMap;
-//    private final HashMap<Integer, Integer> bValuesMap;
+//    private final HashMap<Integer, Integer> aValuesMap = new HashMap<>();
+//    private final HashMap<Integer, Integer> bValuesMap = new HashMap<>();
+//    private final Queue<Process> readyQueue = new LinkedList<>();
 //    private final SlaveCore[] slaveCores;
 //
-//    public MasterCore(SharedMemory sharedMemory, int coreCount) {
-//        this.sharedMemory = sharedMemory;
-//        this.coreCount = coreCount;
-//        this.readyQueue = new LinkedList<>();
-//        this.aValuesMap = new HashMap<>();
-//        this.bValuesMap = new HashMap<>();
-//        this.slaveCores = new SlaveCore[coreCount];
+//    public MasterCore(int coreCount) {
+//        slaveCores = new SlaveCore[coreCount];
 //        for (int i = 0; i < coreCount; i++) {
-//            slaveCores[i] = new SlaveCore(i, sharedMemory, this);
+//            slaveCores[i] = new SlaveCore(i, this);
 //            slaveCores[i].start();
 //        }
 //    }
@@ -115,46 +112,20 @@ public class MasterCore {
 //        readyQueue.add(process);
 //    }
 //
-//    public void printReadyQueue() {
-//        System.out.println("Ready Queue:");
-//        for (Process process : readyQueue) {
-//            System.out.println(process);
-//        }
+//    public Queue<Process> getReadyQueue() {
+//        return readyQueue;
 //    }
 //
 //    public void startScheduling() {
-//        while (!readyQueue.isEmpty() || anyCoreBusy()) {
+//        while (!readyQueue.isEmpty()) {
 //            for (SlaveCore core : slaveCores) {
 //                if (core.isIdle() && !readyQueue.isEmpty()) {
-//                    Process process = readyQueue.poll();
-//                    core.assignProcess(process, 5); // Assign a quantum of 5 for example
+//                    core.assignProcess(readyQueue.poll(), 2); // Example quantum value
 //                }
 //            }
 //        }
-//        for (SlaveCore core : slaveCores) {
-//            core.terminate();
-//        }
 //    }
 //
-//    private boolean anyCoreBusy() {
-//        for (SlaveCore core : slaveCores) {
-//            if (!core.isIdle()) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-////    public void updateValueMap(char variable, int value, Process process) {
-////        if (variable == 'a') {
-////            aValuesMap.put(process.getProcessId(), value);
-////        } else if (variable == 'b') {
-////            bValuesMap.put(process.getProcessId(), value);
-////        }
-////    }
-//
-//    // MasterCore.java
-//    // MasterCore.java
 //    public void updateValueMap(char variable, int value, Process process) {
 //        if (variable == 'a') {
 //            aValuesMap.put(process.getProcessId(), value);
@@ -162,11 +133,6 @@ public class MasterCore {
 //            bValuesMap.put(process.getProcessId(), value);
 //        }
 //        printValueMaps();
-//    }
-//
-//    private void printValueMaps() {
-//        System.out.println("aValuesMap: " + aValuesMap);
-//        System.out.println("bValuesMap: " + bValuesMap);
 //    }
 //
 //    public Integer getAValueByProcessId(int processId) {
@@ -177,8 +143,25 @@ public class MasterCore {
 //        return bValuesMap.get(processId);
 //    }
 //
-//    public Queue<Process> getReadyQueue() {
-//        return readyQueue;
+//    public Integer getValue(char variable) {
+//        if (variable == 'a') {
+//            return aValuesMap.values().stream().findFirst().orElse(null);
+//        } else if (variable == 'b') {
+//            return bValuesMap.values().stream().findFirst().orElse(null);
+//        }
+//        return null;
+//    }
+//
+//    private void printValueMaps() {
+//        System.out.println("aValuesMap: " + aValuesMap);
+//        System.out.println("bValuesMap: " + bValuesMap);
+//    }
+//
+//        public void printReadyQueue() {
+//        System.out.println("Ready Queue:");
+//        for (Process process : readyQueue) {
+//            System.out.println(process);
+//        }
 //    }
 //}
 //
@@ -190,6 +173,34 @@ public class MasterCore {
 //
 //
 //
+////// MasterCore.java
+////package trial;
+////
+////import java.util.HashMap;
+////import java.util.LinkedList;
+////import java.util.Queue;
+////
+////public class MasterCore {
+////    private final SharedMemory sharedMemory;
+////    private final int coreCount;
+////    private final Queue<Process> readyQueue;
+////    private final HashMap<Integer, Integer> aValuesMap;
+////    private final HashMap<Integer, Integer> bValuesMap;
+////    private final SlaveCore[] slaveCores;
+////
+////    public MasterCore(SharedMemory sharedMemory, int coreCount) {
+////        this.sharedMemory = sharedMemory;
+////        this.coreCount = coreCount;
+////        this.readyQueue = new LinkedList<>();
+////        this.aValuesMap = new HashMap<>();
+////        this.bValuesMap = new HashMap<>();
+////        this.slaveCores = new SlaveCore[coreCount];
+////        for (int i = 0; i < coreCount; i++) {
+////            slaveCores[i] = new SlaveCore(i, sharedMemory, this);
+////            slaveCores[i].start();
+////        }
+////    }
+////
 ////    public void addProcess(Process process) {
 ////        readyQueue.add(process);
 ////    }
@@ -224,12 +235,28 @@ public class MasterCore {
 ////        return false;
 ////    }
 ////
+//////    public void updateValueMap(char variable, int value, Process process) {
+//////        if (variable == 'a') {
+//////            aValuesMap.put(process.getProcessId(), value);
+//////        } else if (variable == 'b') {
+//////            bValuesMap.put(process.getProcessId(), value);
+//////        }
+//////    }
+////
+////    // MasterCore.java
+////    // MasterCore.java
 ////    public void updateValueMap(char variable, int value, Process process) {
 ////        if (variable == 'a') {
 ////            aValuesMap.put(process.getProcessId(), value);
 ////        } else if (variable == 'b') {
-////            bValuesMap.put(value, process);
+////            bValuesMap.put(process.getProcessId(), value);
 ////        }
+////        printValueMaps();
+////    }
+////
+////    private void printValueMaps() {
+////        System.out.println("aValuesMap: " + aValuesMap);
+////        System.out.println("bValuesMap: " + bValuesMap);
 ////    }
 ////
 ////    public Integer getAValueByProcessId(int processId) {
@@ -238,10 +265,6 @@ public class MasterCore {
 ////
 ////    public Integer getBValueByProcessId(int processId) {
 ////        return bValuesMap.get(processId);
-////    }
-////
-////    public Process getProcessByBValue(int value) {
-////        return bValuesMap.get(value);
 ////    }
 ////
 ////    public Queue<Process> getReadyQueue() {
@@ -254,31 +277,9 @@ public class MasterCore {
 ////
 ////
 ////
-//////import java.util.HashMap;
-//////import java.util.LinkedList;
-//////import java.util.Queue;
-//////
-//////public class MasterCore {
-//////    private final SharedMemory sharedMemory;
-//////    private final int coreCount;
-//////    private final Queue<Process> readyQueue;
-//////    private final HashMap<Integer, Integer> aValuesMap;
-//////    private final HashMap<Integer, Integer> bValuesMap;
-//////    private final SlaveCore[] slaveCores;
-//////
-//////    public MasterCore(SharedMemory sharedMemory, int coreCount) {
-//////        this.sharedMemory = sharedMemory;
-//////        this.coreCount = coreCount;
-//////        this.readyQueue = new LinkedList<>();
-//////        this.aValuesMap = new HashMap<>();
-//////        this.bValuesMap = new HashMap<>();
-//////        this.slaveCores = new SlaveCore[coreCount];
-//////        for (int i = 0; i < coreCount; i++) {
-//////            slaveCores[i] = new SlaveCore(i, sharedMemory, this);
-//////            slaveCores[i].start();
-//////        }
-//////    }
-//////
+////
+////
+////
 //////    public void addProcess(Process process) {
 //////        readyQueue.add(process);
 //////    }
@@ -315,14 +316,18 @@ public class MasterCore {
 //////
 //////    public void updateValueMap(char variable, int value, Process process) {
 //////        if (variable == 'a') {
-//////            aValuesMap.put(value, process);
+//////            aValuesMap.put(process.getProcessId(), value);
 //////        } else if (variable == 'b') {
 //////            bValuesMap.put(value, process);
 //////        }
 //////    }
 //////
-//////    public Process getProcessByAValue(int value) {
-//////        return aValuesMap.get(value);
+//////    public Integer getAValueByProcessId(int processId) {
+//////        return aValuesMap.get(processId);
+//////    }
+//////
+//////    public Integer getBValueByProcessId(int processId) {
+//////        return bValuesMap.get(processId);
 //////    }
 //////
 //////    public Process getProcessByBValue(int value) {
@@ -339,6 +344,91 @@ public class MasterCore {
 //////
 //////
 //////
-//////
-//////
-//////
+////////import java.util.HashMap;
+////////import java.util.LinkedList;
+////////import java.util.Queue;
+////////
+////////public class MasterCore {
+////////    private final SharedMemory sharedMemory;
+////////    private final int coreCount;
+////////    private final Queue<Process> readyQueue;
+////////    private final HashMap<Integer, Integer> aValuesMap;
+////////    private final HashMap<Integer, Integer> bValuesMap;
+////////    private final SlaveCore[] slaveCores;
+////////
+////////    public MasterCore(SharedMemory sharedMemory, int coreCount) {
+////////        this.sharedMemory = sharedMemory;
+////////        this.coreCount = coreCount;
+////////        this.readyQueue = new LinkedList<>();
+////////        this.aValuesMap = new HashMap<>();
+////////        this.bValuesMap = new HashMap<>();
+////////        this.slaveCores = new SlaveCore[coreCount];
+////////        for (int i = 0; i < coreCount; i++) {
+////////            slaveCores[i] = new SlaveCore(i, sharedMemory, this);
+////////            slaveCores[i].start();
+////////        }
+////////    }
+////////
+////////    public void addProcess(Process process) {
+////////        readyQueue.add(process);
+////////    }
+////////
+////////    public void printReadyQueue() {
+////////        System.out.println("Ready Queue:");
+////////        for (Process process : readyQueue) {
+////////            System.out.println(process);
+////////        }
+////////    }
+////////
+////////    public void startScheduling() {
+////////        while (!readyQueue.isEmpty() || anyCoreBusy()) {
+////////            for (SlaveCore core : slaveCores) {
+////////                if (core.isIdle() && !readyQueue.isEmpty()) {
+////////                    Process process = readyQueue.poll();
+////////                    core.assignProcess(process, 5); // Assign a quantum of 5 for example
+////////                }
+////////            }
+////////        }
+////////        for (SlaveCore core : slaveCores) {
+////////            core.terminate();
+////////        }
+////////    }
+////////
+////////    private boolean anyCoreBusy() {
+////////        for (SlaveCore core : slaveCores) {
+////////            if (!core.isIdle()) {
+////////                return true;
+////////            }
+////////        }
+////////        return false;
+////////    }
+////////
+////////    public void updateValueMap(char variable, int value, Process process) {
+////////        if (variable == 'a') {
+////////            aValuesMap.put(value, process);
+////////        } else if (variable == 'b') {
+////////            bValuesMap.put(value, process);
+////////        }
+////////    }
+////////
+////////    public Process getProcessByAValue(int value) {
+////////        return aValuesMap.get(value);
+////////    }
+////////
+////////    public Process getProcessByBValue(int value) {
+////////        return bValuesMap.get(value);
+////////    }
+////////
+////////    public Queue<Process> getReadyQueue() {
+////////        return readyQueue;
+////////    }
+////////}
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
+////////
--- src/trial/SlaveCore.java
+++ src/trial/SlaveCore.java
@@ -1,3 +1,4 @@
+// SlaveCore.java
 package trial;
 
 public class SlaveCore extends Thread {
@@ -111,9 +112,9 @@ public class SlaveCore extends Thread {
             case "divide":
                 performArithmetic(command, args);
                 break;
-//            case "print":
-//                System.out.println(currentProcess.getProcessId() + args[0] + " = " + masterCore.getValue(args[0].charAt(0)));
-//                break;
+            case "print":
+                System.out.println(currentProcess.getProcessId() + args[0] + " = " + getOperandValue(args[0].charAt(0)));
+                break;
         }
     }
 
@@ -126,6 +127,10 @@ public class SlaveCore extends Thread {
     private void performArithmetic(String operation, String[] args) {
         int operand1 = getOperandValue(args[1].charAt(0));
         int operand2 = getOperandValue(args[2].charAt(0));
+
+        // Print the values being used in the operation
+        System.out.println("Performing " + operation + " with values: " + operand1 + " and " + operand2);
+
         int result = switch (operation) {
             case "add" -> operand1 + operand2;
             case "subtract" -> operand1 - operand2;
@@ -140,14 +145,30 @@ public class SlaveCore extends Thread {
         masterCore.updateValueMap(args[0].charAt(0), result, currentProcess);
     }
 
+//    private void performArithmetic(String operation, String[] args) {
+//        int operand1 = getOperandValue(args[1].charAt(0));
+//        int operand2 = getOperandValue(args[2].charAt(0));
+//        int result = switch (operation) {
+//            case "add" -> operand1 + operand2;
+//            case "subtract" -> operand1 - operand2;
+//            case "multiply" -> operand1 * operand2;
+//            case "divide" -> operand2 != 0 ? operand1 / operand2 : 0;
+//            default -> 0;
+//        };
+//
+//        if (operation.equals("divide") && operand2 == 0) {
+//            System.out.println("Error: Division by zero");
+//        }
+//        masterCore.updateValueMap(args[0].charAt(0), result, currentProcess);
+//    }
+
     private int getOperandValue(char variable) {
         Integer value = null;
         if (variable == 'a') {
             value = masterCore.getAValueByProcessId(currentProcess.getProcessId());
         } else if (variable == 'b') {
             value = masterCore.getBValueByProcessId(currentProcess.getProcessId());
-        }
-        else {
+        } else {
             value = masterCore.getValue(variable);
         }
 
@@ -166,21 +187,22 @@ public class SlaveCore extends Thread {
 
 
 
-//// SlaveCore.java
+
+
+
+
 //package trial;
 //
 //public class SlaveCore extends Thread {
 //    private final int coreId;
-//    private final SharedMemory memory;
 //    private final MasterCore masterCore;
 //    private Process currentProcess;
 //    private volatile boolean isIdle = true;
 //    private volatile boolean terminate = false;
 //    private int quantum;
 //
-//    public SlaveCore(int coreId, SharedMemory memory, MasterCore masterCore) {
+//    public SlaveCore(int coreId, MasterCore masterCore) {
 //        this.coreId = coreId;
-//        this.memory = memory;
 //        this.masterCore = masterCore;
 //    }
 //
@@ -229,9 +251,6 @@ public class SlaveCore extends Thread {
 //        }
 //        System.out.println("Core " + coreId + " terminated.");
 //    }
-//
-//
-//
 //
 //    private void executeCurrentProcess() {
 //        System.out.println("Core " + coreId + " executing Process " + currentProcess.getProcessId());
@@ -286,7 +305,7 @@ public class SlaveCore extends Thread {
 //                performArithmetic(command, args);
 //                break;
 //            case "print":
-//                System.out.println(currentProcess.getProcessId() + args[0] + " = " + memory.get(args[0].charAt(0)));
+//                System.out.println(currentProcess.getProcessId() + args[0] + " = " + masterCore.getValue(args[0].charAt(0)));
 //                break;
 //        }
 //    }
@@ -294,7 +313,6 @@ public class SlaveCore extends Thread {
 //    private void handleInput(String variable) {
 //        System.out.print("Enter value for " + variable + ": ");
 //        int value = new java.util.Scanner(System.in).nextInt();
-//        memory.assign(variable.charAt(0), value);
 //        masterCore.updateValueMap(variable.charAt(0), value, currentProcess);
 //    }
 //
@@ -312,10 +330,167 @@ public class SlaveCore extends Thread {
 //        if (operation.equals("divide") && operand2 == 0) {
 //            System.out.println("Error: Division by zero");
 //        }
-//        memory.assign(args[0].charAt(0), result);
 //        masterCore.updateValueMap(args[0].charAt(0), result, currentProcess);
 //    }
 //
+//    private int getOperandValue(char variable) {
+//        Integer value = null;
+//        if (variable == 'a') {
+//            value = masterCore.getAValueByProcessId(currentProcess.getProcessId());
+//        } else if (variable == 'b') {
+//            value = masterCore.getBValueByProcessId(currentProcess.getProcessId());
+//        }
+//        else {
+//            value = masterCore.getValue(variable);
+//        }
+//
+//        if (value == null) {
+//            throw new IllegalStateException("Value for variable " + variable + " is not set for process ID " + currentProcess.getProcessId());
+//        }
+//
+//        return value;
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+////// SlaveCore.java
+////package trial;
+////
+////public class SlaveCore extends Thread {
+////    private final int coreId;
+////    private final SharedMemory memory;
+////    private final MasterCore masterCore;
+////    private Process currentProcess;
+////    private volatile boolean isIdle = true;
+////    private volatile boolean terminate = false;
+////    private int quantum;
+////
+////    public SlaveCore(int coreId, SharedMemory memory, MasterCore masterCore) {
+////        this.coreId = coreId;
+////        this.memory = memory;
+////        this.masterCore = masterCore;
+////    }
+////
+////    public synchronized void terminate() {
+////        terminate = true;
+////        notify();
+////    }
+////
+////    public synchronized void assignProcess(Process process, int quantum) {
+////        this.currentProcess = process;
+////        this.quantum = quantum;
+////        isIdle = false;
+////        notify();
+////    }
+////
+////    public boolean isIdle() {
+////        return isIdle;
+////    }
+////
+////    public synchronized Process getCurrentProcess() {
+////        return currentProcess;
+////    }
+////
+////    public void makeIdle() {
+////        this.currentProcess = null;
+////        this.isIdle = true;
+////    }
+////
+////    @Override
+////    public void run() {
+////        while (!terminate) {
+////            synchronized (this) {
+////                while (isIdle && !terminate) {
+////                    try {
+////                        wait();
+////                    } catch (InterruptedException e) {
+////                        System.err.println("Core " + coreId + " interrupted.");
+////                        return;
+////                    }
+////                }
+////            }
+////
+////            if (!terminate && currentProcess != null) {
+////                executeCurrentProcess();
+////            }
+////        }
+////        System.out.println("Core " + coreId + " terminated.");
+////    }
+////
+////
+////
+////
+////    private void executeCurrentProcess() {
+////        System.out.println("Core " + coreId + " executing Process " + currentProcess.getProcessId());
+////
+////        int executedInstructions = 0;
+////
+////        // Execute instructions as per quantum
+////        while (executedInstructions < quantum && currentProcess.hasNextInstruction()) {
+////            Instruction instruction = currentProcess.getNextInstruction();
+////            executeInstruction(instruction);
+////            executedInstructions++;
+////        }
+////
+////        // Check if process is completed
+////        if (!currentProcess.hasNextInstruction()) {
+////            System.out.println("Core " + coreId + " completed Process " + currentProcess.getProcessId());
+////            currentProcess.markComplete();
+////        } else {
+////            System.out.println("Core " + coreId + " quantum expired for Process " + currentProcess.getProcessId());
+////            printReadyQueue();
+////        }
+////
+////        // Return the process to the ready queue if not completed
+////        makeIdle();
+////    }
+////
+////    private void printReadyQueue() {
+////        System.out.print("Ready Queue: ");
+////        for (Process process : masterCore.getReadyQueue()) {
+////            String nextInstruction = process.hasNextInstruction() ? process.getInstructions().peek().toString() : "None";
+////            System.out.print("Process " + process.getProcessId() + " [Next: " + nextInstruction + "] ");
+////        }
+////        System.out.println();
+////    }
+////
+////    private void executeInstruction(Instruction instruction) {
+////        String command = instruction.getCommand();
+////        String[] args = instruction.getArgs();
+////
+////        switch (command) {
+////            case "assign":
+////                if (args[1].equals("input")) {
+////                    handleInput(args[0]);
+////                } else {
+////                    performArithmetic(args[1], args);
+////                }
+////                break;
+////            case "add":
+////            case "subtract":
+////            case "multiply":
+////            case "divide":
+////                performArithmetic(command, args);
+////                break;
+////            case "print":
+////                System.out.println(currentProcess.getProcessId() + args[0] + " = " + memory.get(args[0].charAt(0)));
+////                break;
+////        }
+////    }
+////
+////    private void handleInput(String variable) {
+////        System.out.print("Enter value for " + variable + ": ");
+////        int value = new java.util.Scanner(System.in).nextInt();
+////        memory.assign(variable.charAt(0), value);
+////        masterCore.updateValueMap(variable.charAt(0), value, currentProcess);
+////    }
+////
 ////    private void performArithmetic(String operation, String[] args) {
 ////        int operand1 = getOperandValue(args[1].charAt(0));
 ////        int operand2 = getOperandValue(args[2].charAt(0));
@@ -333,33 +508,51 @@ public class SlaveCore extends Thread {
 ////        memory.assign(args[0].charAt(0), result);
 ////        masterCore.updateValueMap(args[0].charAt(0), result, currentProcess);
 ////    }
-//
-//    private int getOperandValue(char variable) {
-//        Integer value = null;
-//        if (variable == 'a') {
-//            value = masterCore.getAValueByProcessId(currentProcess.getProcessId());
-//        } else if (variable == 'b') {
-//            value = masterCore.getBValueByProcessId(currentProcess.getProcessId());
-//        }
-//        else {
-//            value = memory.get(variable);
-//        }
-//
-//        if (value == null) {
-//            throw new IllegalStateException("Value for variable " + variable + " is not set for process ID " + currentProcess.getProcessId());
-//        }
-//
-//        return value;
-//    }
-//
+////
+//////    private void performArithmetic(String operation, String[] args) {
+//////        int operand1 = getOperandValue(args[1].charAt(0));
+//////        int operand2 = getOperandValue(args[2].charAt(0));
+//////        int result = switch (operation) {
+//////            case "add" -> operand1 + operand2;
+//////            case "subtract" -> operand1 - operand2;
+//////            case "multiply" -> operand1 * operand2;
+//////            case "divide" -> operand2 != 0 ? operand1 / operand2 : 0;
+//////            default -> 0;
+//////        };
+//////
+//////        if (operation.equals("divide") && operand2 == 0) {
+//////            System.out.println("Error: Division by zero");
+//////        }
+//////        memory.assign(args[0].charAt(0), result);
+//////        masterCore.updateValueMap(args[0].charAt(0), result, currentProcess);
+//////    }
+////
 ////    private int getOperandValue(char variable) {
+////        Integer value = null;
 ////        if (variable == 'a') {
-////            return masterCore.getAValueByProcessId(currentProcess.getProcessId());
+////            value = masterCore.getAValueByProcessId(currentProcess.getProcessId());
 ////        } else if (variable == 'b') {
-////            return masterCore.getBValueByProcessId(currentProcess.getProcessId());
-////        } else {
-////            return memory.get(variable);
+////            value = masterCore.getBValueByProcessId(currentProcess.getProcessId());
 ////        }
+////        else {
+////            value = memory.get(variable);
+////        }
+////
+////        if (value == null) {
+////            throw new IllegalStateException("Value for variable " + variable + " is not set for process ID " + currentProcess.getProcessId());
+////        }
+////
+////        return value;
 ////    }
-//
-//}
+////
+//////    private int getOperandValue(char variable) {
+//////        if (variable == 'a') {
+//////            return masterCore.getAValueByProcessId(currentProcess.getProcessId());
+//////        } else if (variable == 'b') {
+//////            return masterCore.getBValueByProcessId(currentProcess.getProcessId());
+//////        } else {
+//////            return memory.get(variable);
+//////        }
+//////    }
+////
+////}
